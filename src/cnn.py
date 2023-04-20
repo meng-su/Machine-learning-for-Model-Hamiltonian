af5dae38088d0c54525e208b640f5116aada3fc2
meng-su <67540450+meng-su@users.noreply.github.com>
2023-04-20 10:39:20 +0800

    Update cnn.py

--- src/cnn.py
+++ src/cnn.py
@@ -7,28 +7,27 @@ def build_model(x_in):
 
     model = keras.Sequential(name="cnn")
 
-    model.add(layers.Conv2D(filters=80, kernel_size=2, activation='relu', input_shape=(n_features1, n_features2, n_features3)))
-    # # model.add(layers.AveragePooling2D(pool_size=2,padding="same"))
-    model.add(layers.AveragePooling2D(pool_size=2))
-    # model.add(layers.MaxPooling2D(pool_size=2))
+    model.add(layers.Conv2D(filters=80, kernel_size=2, activation='relu', input_shape=(n_features1, n_features2, n_features3),data_format='channels_first'))
+    # # model.add(layers.AveragePooling2D(pool_size=2,padding="same",data_format='channels_first'))
+    model.add(layers.AveragePooling2D(pool_size=2,data_format='channels_first'))
+    # model.add(layers.MaxPooling2D(pool_size=2,data_format='channels_first'))
 
-    model.add(layers.Conv2D(filters=40, kernel_size=2, activation='relu'))
-    model.add(layers.AveragePooling2D(pool_size=2))
-    # # model.add(layers.AveragePooling2D(pool_size=2,padding="same"))
-    # model.add(layers.MaxPooling2D(pool_size=2))
+    model.add(layers.Conv2D(filters=40, kernel_size=2, activation='relu',data_format='channels_first'))
+    # # model.add(layers.AveragePooling2D(pool_size=2,padding="same",data_format='channels_first'))
+    model.add(layers.MaxPooling2D(pool_size=2,data_format='channels_first'))
 
-    model.add(layers.Conv2D(filters=20, kernel_size=1, activation='relu'))
-    # model.add(layers.AveragePooling2D(pool_size=2))
-    # # model.add(layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
-    # # model.add(layers.MaxPooling2D(pool_size=2))
+    model.add(layers.Conv2D(filters=20, kernel_size=1, activation='relu',data_format='channels_first'))
+    # model.add(layers.AveragePooling2D(pool_size=2,data_format='channels_first'))
+
+
+    # # model.add(layers.Conv2D(filters=64, kernel_size=2, padding='same', activation='relu',data_format='channels_first'))
+    # # model.add(layers.MaxPooling2D(pool_size=2,data_format='channels_first'))
     model.add(layers.Flatten())
 
     # model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dense(128, activation='relu'))
-    model.add(layers.Dense(2, activation='relu'))
-    # model.add(layers.Dense(1, activation='relu'))
-    model.add(layers.Dense(1))
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1, activation='relu'))
 
     model.compile(loss='mse',
                   optimizer='adam',
